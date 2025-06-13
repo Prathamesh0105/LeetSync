437750735122bf12d46d8b81725a5409d69e6f92
Prathamesh0105 <149521210+Prathamesh0105@users.noreply.github.com>
2025-06-13 18:43:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (50.00%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -17,17 +17,13 @@ public:
     void flatten(TreeNode* root) {
         if (!root) return;
 
-        // Process right subtree first
         flatten(root->right);
 
-        // Process left subtree
         flatten(root->left);
 
-        // Set the current node's right to prev and left to null
         root->right = prev;
         root->left = nullptr;
 
-        // Update prev to current node
         prev = root;
     }
 };
